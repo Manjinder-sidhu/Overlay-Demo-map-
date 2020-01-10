82ce56fd1d651f5a9148713d8522c43df4905d5d
Manjinder kaur <manusidhu181@gmail.com>
2020-01-10 15:53:13 -0500

    here

--- Overlay Demo(map)/ViewController.swift
+++ Overlay Demo(map)/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,CLLocationManagerDelegate{
+    
+    var locationManager = CLLocationManager()
 
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let places = Place.getPlaces()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+//        locationManager.startUpdatingLocation()
     }
 
-
+    func addAnnotations(){
+        mapView.delegate = self
+        mapView.addAnnotations(places)
+    }
 }
 
+extension ViewController : MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation{
+            
+            return nil
+        }else{
+            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
+            annotationView.image = UIImage(named: "ic_place_2x")
+            return annotationView
+        }
+    }
+    
+}
